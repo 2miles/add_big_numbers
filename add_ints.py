884eef88395c773d6c56564c5b6cb043d2dd0917
miles <miles4599@gmail.com>
2022-01-26 15:13:39 -0800

    add comand line args as ints

--- add_ints.py
+++ add_ints.py
@@ -2,3 +2,12 @@
 
 import sys
 
+print ('Number of args: ', len(sys.argv), 'args.')
+print ('The first arg: ', sys.argv[0])
+print ('The second arg: ', sys.argv[1])
+print ('The second arg: ', sys.argv[2])
+
+sum = 0
+for i in range(1,len(sys.argv)):
+    sum += int(sys.argv[i])
+print ('The sum of the numbers entered is: ', sum)
